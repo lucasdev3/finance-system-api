27e6e2a3df50bad6caecfb25874a6687e80a83dc
lucasdev3 <lucasrepositoriodados@gmail.com>
2022-12-12 21:25:04 -0300

    init commit

---
+++ src/main/java/br/com/lucasdev3/financesystemapi/FinanceSystemApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucasdev3.financesystemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceSystemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucasdev3/financesystemapi/FinanceSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasdev3.financesystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
