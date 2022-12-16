17033284133dcb889845aa3b217dc02d552114aa
lucasdev3 <lucasrepositoriodados@gmail.com>
2022-12-15 23:57:38 -0300

    add cors

---
+++ src/main/java/br/com/lucasdev3/financesystemapi/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package br.com.lucasdev3.financesystemapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(-1);
+    }
+
+}
